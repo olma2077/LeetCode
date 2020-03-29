165670ced2ca54090154c28b1daedc5be9eb85a9
Oleg Matviichuk <ma7hway@gmail.com>
2020-03-30 01:18:00 +0300

    Create Single Number.py

---
+++ Python3/Single Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = set()
+        
+        for i in nums:
+            if i in res:
+                res.remove(i)
+            else:
+                res.add(i)
+                
+        return res.pop()
